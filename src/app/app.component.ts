69bde0588048fbb102b4e6d54db6cac7ac16cc90
Vincetita <vince.tita@gmail.com>
2018-01-29 02:06:31 +0100

    added home component, string interpolation,property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template: '<h1>{{name}}</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
-  name='Vince';
+
 }
